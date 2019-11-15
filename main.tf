fe10270b35bfdde2c1fc47a5533626c7d0e0bc56
daichi nb <nd940501@gmail.com>
2019-11-15 22:08:45 +0900

    add ec2 instance , subnet

--- main.tf
+++ main.tf
@@ -1,17 +1,36 @@
 // Provider specific configs
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "ap-northeast-1"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region = "ap-northeast-1"
 }
 
-resource "aws_vpc" "myTestVPC" {
-    cidr_block = "10.1.0.0/16"
-    instance_tenancy = "default"
-    enable_dns_support = "true"
-    enable_dns_hostnames = "false"
-    tags = {
-      Name = "myTestVPC"
-    }
+resource "aws_vpc" "myTestVPC_for_terraform" {
+  cidr_block = "10.1.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_support = "true"
+  enable_dns_hostnames = "false"
+  tags = {
+    Name = "myTestVPC_for_terraform"
+  }
 }
+
+resource "aws_subnet" "mySubnet01_for_terraform" {
+  vpc_id = "${aws_vpc.myTestVPC_for_terraform.id}"
+  cidr_block = "10.1.1.0/24"
+  tags = {
+    Name = "mySubnet01_for_terraform"
+  }  
+}
+
+
+resource "aws_instance" "myTestEC2_for_terraform" {
+  instance_type = "t2.micro"
+  ami = "${var.ami_id}"
+  subnet_id = "${aws_subnet.mySubnet01_for_terraform.id}"
+  tags = {
+    Name = "myTestEC2_for_terraform"
+  }
+}
+
 
--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
 // Variables for providers used in this module
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+variable "ami_id" {}
